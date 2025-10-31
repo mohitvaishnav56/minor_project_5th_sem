53c7ad57c03a456ea0236c443d752316b1540166
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2025-10-31 21:44:34 +0600

    navbar file created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom"
 import MainRouter from "./routes/MainRouter"
+import NavBar from "./components/NavBar"
 function App() {
 
   return (
     <>
-      <div className="w-full bg-zinc-700 p-4 text-white text-center">NavBar Goes Here</div>
+      <NavBar />
       <BrowserRouter>
         <MainRouter />
       </BrowserRouter>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+import { CiSearch, CiMenuBurger } from "react-icons/ci";
+const NavBar = () => {
+    return (
+        <>
+            hello
+        </>
+    )
+}
+
+export default NavBar
