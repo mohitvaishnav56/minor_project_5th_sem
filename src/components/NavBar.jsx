076f034eec08df6c10b697860dca1b27e69b06ce
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2025-11-02 14:35:10 +0600

    login page draft 1 done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,9 @@ import CustomButton from "./customElems/CustomButton";
 const NavBar = () => {
   return (
     <>
-        NavBar
+        <div className="w-full h-10vh md:h-[5vh]">
+          NavBar
+        </div>
     </>
   )
 }
--- src/components/customElems/CustomButton.jsx
+++ src/components/customElems/CustomButton.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CustomButton = ({ title, className = '' }) => {
   return (
     <button
-      className={`px-2 py-1 bg-[#ff0000] text-xs cursor-pointer rounded-md text-white ${className}`}
+      className={`px-2 py-2 bg-[#ff0000] text-xs cursor-pointer rounded-md text-white ${className}`}
     >
       {title}
     </button>
