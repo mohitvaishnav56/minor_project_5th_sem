fcf16267bae36deafad01d50a737161069facf5c
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2025-11-05 20:52:19 +0600

    BTL section draft 1

--- src/components/specific/lookbook/LookBookSection.jsx
+++ src/components/specific/lookbook/LookBookSection.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import LBCardContainer from './LBCardContainer';
 const LookBookSection = () => {
-    const [lookBookItems, setLookBookItems] = useState([
+    const [lookBookItems] = useState([
         { id: 1, title: 'Urban Armor', tagline: 'Own the cold. Command the streets.', imageUrl: '/Urban_Armor.jpeg', btnText: 'Shop The Fit' },
         { id: 2, title: 'Midnight Code', tagline: 'Own the cold. Command the streets.', imageUrl: '/Midnight_Code.png', btnText: 'Own This Style' },
         { id: 3, title: 'Effortless Edge', tagline: 'Own the cold. Command the streets.', imageUrl: '/Effertless_Edge.jpeg', btnText: 'Deconstruct The Look' },
@@ -9,14 +9,16 @@ const LookBookSection = () => {
     ]);
 
     return (
-        <div className=" w-full bg-black h-auto md:h-screen py-4 px-2">
-            <h1 className="text-3xl text-white inter-base underline underline-offset-6 decoration-[#FF0000BA]">The LookBook</h1>
-            <div className="w-full md:p-0 gap-2 md:gap-4 flex justify-center flex-wrap md:flex-nowrap h-auto md:h-full ">
-                {
-                    lookBookItems.map((item, index) => (
-                        <LBCardContainer key={item.id} index={index} details={{ ...item }} />
-                    ))
-                }
+        <div className='md:px-8'>
+            <div className=" w-full bg-black h-auto md:h-screen py-4 px-2">
+                <h1 className="text-3xl text-white inter-base underline underline-offset-6 decoration-[#FF0000BA]">The LookBook</h1>
+                <div className="w-full md:p-0 gap-2 md:gap-4 flex justify-center flex-wrap md:flex-nowrap h-auto md:h-full ">
+                    {
+                        lookBookItems.map((item, index) => (
+                            <LBCardContainer key={item.id} index={index} details={{ ...item }} />
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../components/specific/home/hero/Hero'
 import LookBookSection from '../components/specific/lookbook/LookBookSection'
+import BTL from '../components/specific/btl/BTL'
 const Home = () => {
   return (
-    <div className='md:px-8 bg-black'>
+    <div className='bg-black'>
         <Hero />
         <LookBookSection />
+        <BTL />
     </div>
   )
 }
