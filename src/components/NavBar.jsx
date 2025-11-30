e5fc9306c38af326520824f6312ea29b2cf17bee
Khushi Patidar <khushipatidar.013@gmail.com>
2025-11-30 11:28:22 +0530

    1st Section

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,60 +1,76 @@
-
 import CustomLink from "./customElems/CustomLink";
 import CustomButton from "./customElems/CustomButton";
 import { CiSearch } from "react-icons/ci";
 import { CiMenuBurger } from "react-icons/ci";
 import { useState } from "react";
 const NavBar = () => {
-   const [isOpen,setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-       <div className="relative bg-black p-6 flex items-center justify-between">
-    <div className="text-white font-[Inria_Serif] tracking-[5px] underline underline-offset-[6px] decoration-[#ff0000] ">
-      GANGSTER
-      </div>
-      <ul className="hidden md:flex gap-9">
-         
-    <CustomLink href="/" title="Home" />
-  <CustomLink href="/" title="Shop" />
-  <CustomLink href="/" title="Collections" />
-  <CustomLink href="/" title="About" />
-  <CustomLink href="/" title="Contact" />
-  </ul>
-  <div className=" hidden md:flex gap-2 items-center ">
-    <div style={{alignItems: "center"}} className="text-white flex gap-2 rounded item-center border border-white px-2  py-1 ">
-      <CiSearch className="h-fit"/>
-      <input type="text" placeholder="Search..." className="w-[100px] bg-black outline-none text-white placeholder:text-sm" ></input>
-    </div>
-    <CustomLink href={"/login"} title={<CustomButton title="login"  />}/>
-  </div>
-<div className="text-white font-2xl flex gap-2 items-center  md:hidden">
-  <CiSearch className="cursor-pointer"/>
-  <CiMenuBurger className="cursor-pointer" 
-  onClick={()=>setIsOpen(!isOpen)}/>
-</div>
-
-       </div>
-       {isOpen && (
-        <>
-        <div className="absolute z-4 transition-3  h-screen  bg-black bg-opacity-40 flex justify-end  md:hidden">
-          <div className="h-full w-70 bg-red-400 ">
-            <ul className="flex flex-col gap-4 items-center p-5 ">
-    <li>   
-    <CustomLink href="/" title="Home" /></li>  
-  <li> <CustomLink href="/" title="Shop" /></li>  
-   <li><CustomLink href="/" title="Collections" /></li>  
-   <li><CustomLink href="/" title="About" /></li>  
-   <li><CustomLink href="/" title="Contact" /></li>  
-   <li><CustomButton title="login" /></li>
-  </ul>
-
-          </div>
+      <div className="relative bg-black p-6 flex items-center justify-between w-screen">
+        <div className="text-white font-[Inria_Serif] tracking-[5px] underline underline-offset-[6px] decoration-[#ff0000] ">
+          GANGSTER
         </div>
-          </>
+        <ul className="hidden md:flex md:gap-9">
+          <CustomLink href="/" title="Home" />
+          <CustomLink href="/" title="Shop" />
+          <CustomLink href="/" title="Collections" />
+          <CustomLink href="/" title="About" />
+          <CustomLink href="/" title="Contact" />
+        </ul>
+        <div className=" hidden md:flex md:gap-2 md:items-center ">
+          <div
+            style={{ alignItems: "center" }}
+            className="text-white flex gap-2 rounded item-center border border-white px-2  py-1 "
+          >
+            <CiSearch className="h-fit " />
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-[100px] bg-black outline-none text-white placeholder:text-sm"
+            ></input>
+          </div>
+          <CustomLink href={"/login"} title={<CustomButton title="login" className="w-30"/>} />
+        </div>
+        <div className="text-white font-2xl flex gap-2 items-center  md:hidden">
+          <CiSearch className="cursor-pointer" />
+          <CiMenuBurger
+            className="cursor-pointer mr-0"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </div>
+      </div>
+      {isOpen && (
+        <>
+          <div className="absolute z-4 transition-all-3  h-screen  bg-black bg-opacity-40 flex justify-end  md:hidden">
+            <div className="h-full w-70 bg-red-400 ">
+              <ul className="flex flex-col gap-4 items-center p-5 ">
+                <li>
+                  <CustomLink href="/" title="Home" />
+                </li>
+                <li>
+                  {" "}
+                  <CustomLink href="/" title="Shop" />
+                </li>
+                <li>
+                  <CustomLink href="/" title="Collections" />
+                </li>
+                <li>
+                  <CustomLink href="/" title="About" />
+                </li>
+                <li>
+                  <CustomLink href="/" title="Contact" />
+                </li>
+                <li>
+                  <CustomButton title="login" />
+                </li>
+              </ul>
+            </div>
+          </div>
+        </>
       )}
-
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/specific/home/hero/Hero.jsx
+++ src/components/specific/home/hero/Hero.jsx
@@ -1,38 +1,63 @@
-import React from 'react'
-import CustomPara from '../../../customElems/CustomPara'
-import CustomButton from '../../../customElems/CustomButton'
+import React from "react";
+import CustomPara from "../../../customElems/CustomPara";
+import CustomButton from "../../../customElems/CustomButton";
 const Hero = () => {
   return (
-    <> 
-    <div className="h-screen w-screen bg-blue-400  
-    md: h-200 md:w-360 md:bg-red-500">
-<div className=' test-font ml-40  '>
-  Gang
-  </div>
+    <>
+      <div
+        className="h-550 mr-5
+    md: h-screen md:mr-0 relative"
+      >
+        <div
+          className="  test-font text-7xl absolute top-25 left-4
+ md:test-font   md:text-[16em]  md:top-10 md:left-70 md:tracking-[4px]"
+        >
+          Gang
+        </div>
 
+        <img
+          src="/first.png"
+          alt="hero"
+          className="absolute top-10 left-15 h-70 brightness-50 w-60
+         md:top-13 md:left-133 md:brightness-50 md:h-135 md:w-115"
+        />
 
-        <img src="/first.png" alt="hero" className='absolute top-55 left-130 brightness-50' />
-
-        <div className='text-white font-bold text-4xl font-[Inter]  border-b-2 border-b-red-600 absolute left-60 top-107'>
+        <div
+          className=" whitespace-nowrap text-white font-bold text-[8px] font-[Inter]  border-b-2 border-b-red-600 absolute left-5 top-39
+        md:text-3xl md:left-76 md:top-62"
+        >
           Bold. Fearless. Untamed.
         </div>
 
-        <CustomPara className=' absolute left-60 top-130 w-85'
+        <CustomPara
+          className=" absolute top-90 left-2 w-90 text-[14px]
+        md:left-75 md:top-78 md:w-85 md:text-[19px]"
           description="Step into a world of raw style and unapologetic attitude. 
- Designed for the fearless, made for the originals."/>
+ Designed for the fearless, made for the originals."
+        />
 
-        <CustomButton title="Join The Crew Now" className='w-80 h-10 absolute top-160 left-60' />
+        <CustomButton
+          title="Join The Crew Now"
+          className="absolute w-40 h-10 top-110 left-22
+        md:w-70 md:h-10  md:top-105 md:left-75"
+        />
 
-        <div className='test-font ml-170 absolute top-108  '>
+        <div
+          className="test-font absolute top-67 left-47 text-7xl
+        md:left-180 md:top-105 md:text-[16em] md:tracking-[4px]  "
+        >
           ster
         </div>
 
-        <div className='text-[#FFFFFF] absolute left-243 top-191 tracking-[3px] tex-5xl font-bold font-[Inter] '>
+        <div
+          className="text-[#FFFFFF] absolute top-81 left-50 text-[8px] tracking-[1px]  font-bold font-[Inter] whitespacing-nowrap 
+       md:left-225 md:top-155 md:text-[19px]"
+        >
           GET NEW FASHION
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
